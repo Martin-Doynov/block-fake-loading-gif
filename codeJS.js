91cec54f278264719a8c28c0fd545bf9e3fe6c7b
Martin-Doynov <137278362+Martin-Doynov@users.noreply.github.com>
2024-05-29 17:07:37 +0300

    Also loads all hidden divs at startup!

--- codeJS.js
+++ codeJS.js
@@ -2,16 +2,25 @@
 var targetNode = document.body;
 
 // Options for the observer (which mutations to observe)
-var config = { childList: true, subtree: true };
+var config = { attributes: true, childList: true, subtree: true };
 
 // Callback function to execute when mutations are observed
 var callback = function(mutationsList, observer) {
     for(let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
+        if (mutation.type === 'childList' || mutation.type === 'attributes') {
             var overlay = document.querySelector('#overlay');
             if (overlay) {
                 overlay.parentNode.removeChild(overlay);
             }
+
+            // Loads by default the hidden divs
+            var divs = ['courses_wrapper', 'subjects_wrapper', 'groups_wrapper'];
+            divs.forEach(function(id) {
+                var div = document.getElementById(id);
+                if (div && div.style.display === 'none') {
+                    div.style.display = 'block';
+                }
+            });
         }
     }
 };
